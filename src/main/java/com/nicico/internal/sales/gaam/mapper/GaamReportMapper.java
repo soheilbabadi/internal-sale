aadd34ae0899f82dfae56159d642a4eb3ed1146a
soheil babadi <babadishyl@gmail.com>
2026-09-13 08:57:43 +0330

    fix model

--- src/main/java/com/nicico/internal/sales/gaam/mapper/GaamReportMapper.java
+++ src/main/java/com/nicico/internal/sales/gaam/mapper/GaamReportMapper.java
@@ -1,14 +1,14 @@
-//package com.nicico.internal.sales.gaam.mapper;
-//
-//
-//import com.nicico.internal.sales.gaam.dto.GaamReportDto;
-//import com.nicico.internal.sales.gaam.model.GaamReportModel;
-//import org.mapstruct.Mapper;
-//
-//@Mapper(componentModel = "spring")
-//public interface GaamReportMapper {
-//	GaamReportDto.Info toDTO(GaamReportModel model);
-//
-//	GaamReportModel fromDTO(GaamReportDto.Create request);
-//
-//}
+package com.nicico.internal.sales.gaam.mapper;
+
+
+import com.nicico.internal.sales.gaam.dto.GaamReportDto;
+import com.nicico.internal.sales.gaam.model.GaamReportModel;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface GaamReportMapper {
+	GaamReportDto.Info toDTO(GaamReportModel model);
+
+	GaamReportModel fromDTO(GaamReportDto.Create request);
+
+}
